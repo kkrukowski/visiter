b277c4310ab58d743c262e3b960bc302a4b3ebc7
Kamil <hadekshd@gmail.com>
2022-11-22 14:31:53 +0100

    Add database connection

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+// Server setup
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 5000;
 
+// Database - mongodb
+const mongoose = require("mongoose");
+
+// Other
 const path = require("path");
 
+// MongoDB connection
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
 app.get("/", function (req, res) {
-  console.log(path.join(__dirname, "/index.html"));
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
