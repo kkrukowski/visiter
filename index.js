fcf33f6cfd0470563cfbb56f52889b5a006f6cd4
Hary <konrad.harezlak@edu.uekat.pl>
2022-11-25 18:09:34 +0100

    rejestracja

--- index.js
+++ index.js
@@ -33,9 +33,29 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
 });
 
-
-
-
+function rejestracja(){
+document.getElementById("sekcja").innerHTML=`<br>
+<img src="pic/logo.PNG">
+<h3>LOGOWANIE</h3>
+<form action="/rejestracja.html" method="get">
+    <input type="email" value="E-mail">
+    <br><br>
+    <input type="text" value="Imie">
+    <br><br>
+    <input type="text" value="Nazwisko">
+    <br><br>
+    <p id="pleclabel">Płeć:</p>
+    <div id="plec">
+    <label><input type="radio" name="plec">Mężczyzna</label>           
+    <label><input type="radio" name="plec">Kobieta</label>
+    </div>
+    <br>
+    <input id="pass" for="pass" type="text" autocomplete="current-password" value="Hasło">
+    <i  class="far fa-eye" id="eye" onclick="pokazhaslo()"></i><br> <br>
+    <input id="submit" type="submit" value="Zarejestruj się"><br><br>
+</form>`
+document.getElementById("sekcja").style.height="auto";
+};
 
 function pokazhaslo() {
   const eye = document.querySelector('#eye');
