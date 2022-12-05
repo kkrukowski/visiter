e2766b4277abf17d1ccc155f7d4a0450ef8813c0
Hary <konrad.harezlak@edu.uekat.pl>
2022-12-05 21:13:53 +0100

    dokonczenie strony logowania, rozpoczecie strony glownej

--- html/js/index.js
+++ html/js/index.js
@@ -1,34 +1,15 @@
-function rejestracja() {
-  document.getElementById("sekcja").innerHTML = `<br>
-    <img src="pic/logo.PNG">
-    <h3>LOGOWANIE</h3>
-    <form action="/rejestracja.html" method="get">
-        <input type="email" value="E-mail">
-        <br><br>
-        <input type="text" value="Imie">
-        <br><br>
-        <input type="text" value="Nazwisko">
-        <br><br>
-        <p id="pleclabel">Płeć:</p>
-        <div id="plec">
-        <label><input type="radio" name="plec">Mężczyzna</label>           
-        <label><input type="radio" name="plec">Kobieta</label>
-        </div>
-        <br>
-        <input id="pass" for="pass" type="text" autocomplete="current-password" value="Hasło">
-        <i  class="far fa-eye" id="eye" onclick="pokazhaslo()"></i><br> <br>
-        <input id="submit" type="submit" value="Zarejestruj się"><br><br>
-    </form>`;
-  document.getElementById("sekcja").style.height = "auto";
-}
-
 function pokazhaslo() {
   const eye = document.querySelector("#eye");
   const pass = document.querySelector("#pass");
-  if (pass.getAttribute("type") === "password") {
-    pass.setAttribute("type", "text");
-  } else {
+  if (pass.getAttribute("type") === "text") {
     pass.setAttribute("type", "password");
+  } else {  
+    pass.setAttribute("type", "text");
   }
   eye.classList.toggle("fa-eye-slash");
+
+}
+function przyp_haslo(){
+document.querySelector("#sekcja_logowanie").innerHTML="<img src='pic/logo.PNG' <h3>Wysłaliśmy kod na adres E-mail:</h3> <label for='email'></label> <input type='email' name='email' autocomplete placeholder='Kod'> <button id='submit' onclick='przyp_haslo()' value='Wyślij'> Wyślij </button>"
+
 }
