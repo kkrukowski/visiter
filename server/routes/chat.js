1b0999a23da136cbfea22752b9c1f62021178676
unknown <hadekshd@gmail.com>
2023-01-16 17:59:28 +0100

    Implement chat to business in search results

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -13,4 +13,14 @@ router.get("/chat", isLoggedIn, (req, res) => {
   });
 });
 
+router.get("/chat/:id", isLoggedIn, (req, res) => {
+  userId = req.user._id.toString();
+  receiverId = req.params.id;
+  res.render("chat", {
+    name: req.user.name,
+    userDbId: userId,
+    receiverId: receiverId,
+  });
+});
+
 module.exports = router;
