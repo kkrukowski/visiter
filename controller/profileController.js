3cea81c727b9fb43160136b78cdae4396d0d1c10
danielpaluch <danielpaluch37@gmail.com>
2023-03-21 17:47:37 +0100

    code refactor in profileController complete

--- controller/profileController.js
+++ controller/profileController.js
@@ -3,136 +3,145 @@ const Opinion = require("../models/OpinionForUser");
 const Business = require("../models/Business");
 const OpinionForBusiness = require("../models/OpinionForBusiness");
 
-const editProfile = (req, res) => {
-    let update;
-    if (req.body.name && req.body.surname) {
-        update = { name: req.body.name, surname: req.body.surname };
-    } else if (req.body.email) {
-        update = { email: req.body.email };
-    } else if (req.body.phone) {
-        var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{3})$/; //validation of phone
-        if (!re.test(req.body.phone)) {
-            const message = "Podaj prawidłowy numer telefonu.";
-            const opinionsIds = user.opinions;
-            Opinion.find({ "_id": { $in: opinionsIds } }).populate(["ownerId", "businessId"]).exec(function (err, opinions) {
-                if (err) return res.redirect("/")
-                return res.render("profile", {
-                    currentUser: req.user,
-                    user: req.user,
-                    opinions,
-                    message: message,
-                    isSameUser: req.params.id == req.user._id ? true : false
-                });
-            });
-        }
-        update = { phone: req.body.phone };
-    } else if (req.body.country) {
-        update = { country: req.body.country };
-    } else if (req.body.city) {
-        update = { city: req.body.city };
-    }
-    User.findByIdAndUpdate(req.params.id, update, { new: true }, (err, user) => {
-        if (err) {
-            const opinionsIds = user.opinions;
-            Opinion.find({ "_id": { $in: opinionsIds } }).populate(["ownerId", "businessId"]).exec(function (err, opinions) {
-                if (err) return res.redirect("/")
-                return res.render("profile", {
-                    currentUser: req.user,
-                    user: user,
-                    opinions,
-                    message: "Edycja niepomyślna",
-                    isSameUser: req.params.id == req.user._id ? true : false
-                });
-            });
-        }
-        else {
-            const opinionsIds = user.opinions;
-            Opinion.find({ "_id": { $in: opinionsIds } }).populate(["ownerId", "businessId"]).exec(function (err, opinions) {
-                if (err) return res.redirect("/")
-                return res.render("profile", {
-                    currentUser: req.user,
-                    user: user,
-                    opinions,
-                    message: "Edycja pomyślna",
-                    isSameUser: req.params.id == req.user._id ? true : false
-                });
-            });
-        }
-    });
-};
+const editProfile = async (req, res) => {
+    try {
+        if (req.user._id == req.params.id) {
+            if (req.body.name && req.body.surname)
+                update = { name: req.body.name, surname: req.body.surname };
+            else if (req.body.email)
+                update = { email: req.body.email };
+            else if (req.body.phone) {
+                var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{3})$/; //validation of phone
+                if (!re.test(req.body.phone)) {
+                    const message = "Podaj prawidłowy numer telefonu.";
+                    const opinions = await Opinion.find({ "_id": { $in: req.user.opinions } }).populate(["ownerId", "businessId"]).exec();
+                    return res.render("profile", {
+                        currentUser: req.user,
+                        user: req.user,
+                        opinions,
+                        message: message,
+                        isSameUser: req.params.id == req.user._id ? true : false
+                    });
+                }
+                update = { phone: req.body.phone };
+            } else if (req.body.country)
+                update = { country: req.body.country };
+            else if (req.body.city)
+                update = { city: req.body.city };
 
-const getUser = (req, res) => {
-    User.findById(req.params.id, (err, user) => {
-        const opinionsIds = user.opinions;
-        Opinion.find({ "_id": { $in: opinionsIds } }).populate(["ownerId", "businessId"]).exec(function (err, opinions) {
-            if (err) return res.redirect("/")
+            const user = await User.findByIdAndUpdate(req.params.id, update, { new: true }).exec();
+            const opinions = await Opinion.find({ "_id": { $in: user.opinions } }).populate(["ownerId", "businessId"]).exec();
             return res.render("profile", {
                 currentUser: req.user,
-                user: user,
-                opinions,
+                user,
+                opinions: opinions,
                 message: "",
                 isSameUser: req.params.id == req.user._id ? true : false
             });
+        }
+    } catch (err) {
+        const user = await User.findById(req.params.id).exec();
+        const opinions = await Opinion.find({ ownerId: req.params.id }, { new: true }).populate(["ownerId", "businessId"]).exec();
+        const message = "Edycja niepomyślna.";
+        return res.render("profile", {
+            currentUser: req.user,
+            user,
+            opinions,
+            message,
+            isSameUser: req.params.id == req.user._id ? true : false
         });
-    });
-};
-
-const addOpinion = (req, res) => {
-    correctName = req.user.name + " " + req.user.surname;
-    if (req.user.role == "Worker") {
-        Business.findOne({ workers: req.user._id, }, (err, business) => {
-            const newOpinion = new Opinion({
-                rating: req.body.rating,
-                comment: req.body.comment,
-                ownerId: req.user._id,
-                businessId: business._id,
-            });
-            newOpinion.save((err, opinion) => {
-                if (err) {
-                    return res.redirect("/") //dodac render
-                } else {
-                    User.findByIdAndUpdate(req.params.id, { $push: { opinions: opinion } }, { new: true }, (err, user) => {
-                        return res.render("")
-                    });
-                };
-            });
-        });
-    } else if (req.user.role == "Owner") {
-        Business.findOne({ ownerId: req.user._id }, (err, business) => {
-            const newOpinion = new Opinion({
-                rating: req.body.rating,
-                comment: req.body.comment,
-                ownerId: req.user._id,
-                businessId: business._id,
-            });
-            newOpinion.save((err, opinion) => {
-                if (err) {
-                    return getUser(req, res);
-                } else {
-                    User.findByIdAndUpdate(req.params.id, { $push: { opinions: opinion } }, { new: true }, (err, user) => {
-                        return getUser(req, res);
-                    });
-                };
-            });
-        });
-    } else {
-        return res.render("home", { user: req.user, message: "Nie jesteś pracownikiem aby wystawić opinie." }); // wyswietl error ze nie jest z firmy
     }
 };
 
-const removeOpinion = (req, res) => {
-    User.findByIdAndUpdate(req.params.id, { $pull: { opinions: req.params.idOpinion } }, { new: true }, (err, user) => {
-        if (err) return getUser(req, res);
-        Opinion.findByIdAndDelete(req.params.idOpinion, { new: true }, (err, opinion) => {
-            return getUser(req, res);
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate("opinions").exec();
+        const opinions = await Opinion.find({ "_id": { $in: user.opinions } }).populate(["ownerId", "businessId"]).exec();
+        return res.render("profile", {
+            currentUser: req.user,
+            user,
+            opinions: opinions,
+            message: "",
+            isSameUser: req.params.id == req.user._id ? true : false
         });
-    });
+    } catch (err) {
+        const user = await User.findById(req.user._id).exec();
+        const message = "Użytkownik nie znaleziony.";
+        return res.render("home", {
+            user,
+            business: req.user.role == "Owner" ? await Business.find({ ownerId: req.user._id }).exec() : await Business.find({ workers: req.user._id }).exec(),
+            //^ wyszukaj dla ownera, jesli nie to dla pracownika, jesli klient nie jest pracownikiem zwroci false.
+            message: message
+        });
+    }
+};
+
+const addOpinion = async (req, res) => {
+    try {
+        correctName = req.user.name + " " + req.user.surname;
+        let business = undefined;
+        if (req.user.role == "Worker") { //znalezienie firmy przez pracownikach
+            business = await Business.findOne({ workers: req.user._id }).exec();
+        } else if (req.user.role == "Owner") { // znalezienie firmy przez wlasciciela
+            business = await Business.findOne({ ownerId: req.user._id }).exec();
+        } else { // uzytkownik jest klientem, klient klientowi nie moze wystawic opinii
+            const user = await User.findById(req.params.id).exec();
+            const opinions = await Opinion.find({ ownerId: req.params.id }, { new: true }).populate(["ownerId", "businessId"]).exec();
+            const message = "Nie jesteś pracownikiem lub właścicielem aby wystawić opinie.";
+            return res.render("profile", {
+                currentUser: req.user,
+                user,
+                opinions,
+                message,
+                isSameUser: req.params.id == req.user._id ? true : false
+            });
+        }
+        const newOpinion = new Opinion({
+            rating: req.body.rating,
+            comment: req.body.comment,
+            ownerId: req.user._id,
+            businessId: business._id
+        });
+        newOpinion.save();
+        await User.findByIdAndUpdate(req.params.id, { $push: { opinions: newOpinion } }, { new: true }).exec();
+        return getUser(req, res);
+    } catch (err) {
+        const user = await User.findById(req.params.id).exec();
+        const opinions = await Opinion.find({ ownerId: req.params.id }, { new: true }).populate(["ownerId", "businessId"]).exec();
+        const message = "Opinia nie została dodana.";
+        return res.render("profile", {
+            currentUser: req.user,
+            user,
+            opinions,
+            message,
+            isSameUser: req.params.id == req.user._id ? true : false
+        });
+    }
+};
+
+const removeOpinion = async (req, res) => {
+    try {
+        await User.findByIdAndUpdate(req.params.id, { $pull: { opinions: req.params.idOpinion } }, { new: true }).exec();
+        await Opinion.findByIdAndDelete(req.params.idOpinion, { new: true }).exec();
+        return getUser(req, res);
+    } catch (err) {
+        const user = await User.findById(req.params.id).exec();
+        const opinions = await Opinion.find({ ownerId: req.params.id }, { new: true }).populate(["ownerId", "businessId"]).exec();
+        const message = "Opinia nie została usunięta.";
+        return res.render("profile", {
+            currentUser: req.user,
+            user,
+            opinions,
+            message,
+            isSameUser: req.params.id == req.user._id ? true : false
+        });
+    }
 };
 
 const removeProfile = async (req, res) => {
-    try{
+    try {
         if (req.params.id == req.user._id) {
-            if (req.user.role == "User") {
+            if (req.user.role == "User") { // dla uzytkownika
                 const opinionIds = await OpinionForBusiness.find({ ownerId: req.params.id }).select("_id").exec();
                 let listOfIds = [];
                 opinionIds.forEach(opinionId => {
@@ -150,21 +159,21 @@ const removeProfile = async (req, res) => {
                 await User.updateMany({ opinions: { $in: listOfUserIds } }, { $pull: { opinions: { $in: listOfUserIds } } }).exec()
                 await OpinionForBusiness.deleteMany({ ownerId: req.params.id }).exec();
                 await Opinion.deleteMany({ ownerId: req.params.id }).exec();
-                await Opinion.deleteMany({ _id: { $in: opinions } }).exec();       
+                await Opinion.deleteMany({ _id: { $in: opinions } }).exec();
                 req.logOut(err => {
                     if (err) return next(err);
                     const message = "Konto usunięte.";
                     return res.render("login", { message: message });
-                });     
+                });
             }
-            else if (req.user.role == "Owner") {
+            else if (req.user.role == "Owner") { // dla wlasciciela
                 Business.findOne({ owner: req.user._id }, (err, business) => {
                     if (err) return res.render("home", { user: req.user, message: "Coś poszło nie tak." });
                     const message = "Aby usunąć konto, usuń najpierw firmę."
                     return res.render("home", { user: req.user, business, message: message });
                 });
             }
-            else {
+            else { // dla workera
                 const opinionIds = await OpinionForBusiness.find({ ownerId: req.params.id }).select("_id").exec();
                 let listOfIds = [];
                 opinionIds.forEach(opinionId => {
