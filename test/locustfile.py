13222de552c0d000335fcd73cf01ae46edf5bca2
danielpaluch <danielpaluch37@gmail.com>
2023-05-16 14:35:45 +0200

    bugfix with performance test

--- test/locustfile.py
+++ test/locustfile.py
@@ -3,3 +3,15 @@ class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/")
+    @task
+    def logIn(self):
+        req = {
+            "username": "test@test.com",
+            "password": "12345"
+        }
+        response = self.client.post("/login", req)
+        print("LogIn code: ", response.request)
+    @task
+    def showBusiness(self):
+        response = self.client.get("/business/search", auth = ("test@test.com", "12345"))
+        print("Search code: ", response)
