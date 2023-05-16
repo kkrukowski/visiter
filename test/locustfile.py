9b930346a29b8e29d24f89d2a83ce0db77413b3d
danielpaluch <danielpaluch37@gmail.com>
2023-05-16 15:14:46 +0200

    functionality change of performance tests

--- test/locustfile.py
+++ test/locustfile.py
@@ -1,17 +1,16 @@
 from locust import HttpUser, task
 class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("/")
-    @task
-    def logIn(self):
+    def on_start(self):
+        self.client.get("/login")
         req = {
             "username": "test@test.com",
             "password": "12345"
         }
-        response = self.client.post("/login", req)
-        print("LogIn code: ", response.request)
+        self.client.post("/login", req)
     @task
     def showBusiness(self):
-        response = self.client.get("/business/search", auth = ("test@test.com", "12345"))
-        print("Search code: ", response)
+        response = self.client.get("/business/search")
+        print(response.text)
+    @task
+    def logout(self):
+        self.client.post("/logout")
